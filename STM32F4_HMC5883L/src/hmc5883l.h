418264cde19a7a8bd46bc7c76aa979e9e723ff0d
Michal Ksiezopolski <xiezopolskim@gmail.com>
2014-05-11 21:24:43 +0200

    Init2

---
+++ STM32F4_HMC5883L/src/hmc5883l.h
@@ -0,0 +1,23 @@
+/**
+ * @file: 	hmc5883l.h
+ * @brief:	   
+ * @date: 	11 maj 2014
+ * @author: Michal Ksiezopolski
+ * 
+ * @verbatim
+ * Copyright (c) 2014 Michal Ksiezopolski.
+ * All rights reserved. This program and the 
+ * accompanying materials are made available 
+ * under the terms of the GNU Public License 
+ * v3.0 which accompanies this distribution, 
+ * and is available at 
+ * http://www.gnu.org/licenses/gpl.html
+ * @endverbatim
+ */
+
+#ifndef HMC5883L_H_
+#define HMC5883L_H_
+
+
+
+#endif /* HMC5883L_H_ */
